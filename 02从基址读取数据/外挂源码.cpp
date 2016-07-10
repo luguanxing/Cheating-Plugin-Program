42a65bdd6247b99a7ef3df42695bec5dfa816f23
luguanxing <651571925@qq.com>
2016-07-10 21:57:36 +0800

    在三级基址获得数据

--- 02从基址读取数据/外挂源码.cpp
+++ 02从基址读取数据/外挂源码.cpp
@@ -2,20 +2,20 @@
 #include <windows.h>
 int main() {
 	
-	HWND h = ::FindWindow(NULL, "植物大战僵尸中文版");  //  寻找并打开进程
+	HWND h = ::FindWindow(NULL, "植物大战僵尸中文版");   //	  寻找并打开进程
 	DWORD processid;
 	GetWindowThreadProcessId(h, &processid);
 	HANDLE processh = 0;
 	processh = OpenProcess(PROCESS_ALL_ACCESS,FALSE,processid);
 	
-	if (processh == 0) {    //  对应处理
+	if (processh == 0) { 	//    对应处理
         printf("打开进程失败!\n");
         return 1;
 	} else {
 		printf("打开进程成功!\n");
 		
-		int sun;	// [[[006A9EC0]+768]+5560] = sun
-		LPCVOID mbase = (LPCVOID)0x006A9EC0;
+		int sun;    //	  用于存放阳光数据
+		LPCVOID mbase = (LPCVOID)0x006A9EC0;	//    [[[006A9EC0]+768]+5560] = sun
 		LPVOID mbuffer = (LPVOID)&sun;
 		::ReadProcessMemory(processh, mbase, mbuffer, 4,  0);
 		mbase = (LPCVOID)(sun + 0x768);
@@ -24,8 +24,8 @@ int main() {
 		::ReadProcessMemory(processh, mbase, mbuffer, 4,  0);
 		printf("您有阳光:%d\n", sun);
 
-		int money;	//	[[[006A9F38]+82C]+28] = money
-		LPCVOID mbase2 = (LPCVOID)0x006A9F38;
+		int money;     //    用于存放钱数据
+		LPCVOID mbase2 = (LPCVOID)0x006A9F38;      //    [[[006A9F38]+82C]+28] = money
 		LPVOID mbuffer2 = (LPVOID)&money;
 		::ReadProcessMemory(processh, mbase2, mbuffer2, 4,  0);
 		mbase2 = (LPCVOID)(money + 0x82C);
