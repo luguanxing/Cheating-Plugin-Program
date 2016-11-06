5546ee9b0fb6b79352c1b45549aa2cd0e14cfafe
luguanxing <651571925@qq.com>
2016-11-06 17:37:18 +0800

    简单DLL劫持

--- 05简单DLL劫持/cheatDLL.cpp
+++ 05简单DLL劫持/cheatDLL.cpp
@@ -1,17 +1,17 @@
 //
-//  05¼òµ¥DLL½Ù³Ö(×÷±×Ä£¿éDLL²¿·Ö)
+//  05ç®€å•DLLåŠ«æŒ(ä½œå¼Šæ¨¡å—DLLéƒ¨åˆ†)
 //  C/C++
 //
 //  Created by luguanxing.
 //  Copyright @2016 LGX. All rights reserved.
 //
 #include <windows.h>
-#define Dllfunciton extern "C" __declspec(dllexport)    //ÒÔC·½Ê½µ¼³ö
+#define Dllfunciton extern "C" __declspec(dllexport)    //ä»¥Cæ–¹å¼å¯¼å‡º
 
 Dllfunciton void lockdata();
 Dllfunciton DWORD WINAPI inject(LPVOID);
 
-void lockdata() {
+void lockdata() {   //é”å®šè¡€é‡åŠŸèƒ½
     while (true) {
         DWORD hp = 10;
         DWORD heart = 99;
@@ -21,7 +21,7 @@ void lockdata() {
         DWORD addr2 = 0x00428292;
         DWORD addr3 = 0x004282a2;
 
-        DWORD res = WriteProcessMemory(INVALID_HANDLE_VALUE, (LPVOID)addr, &hp, 4, 0);  //Ğ´Èë×ÔÉíĞŞ¸ÄÓÎÏ·Êı¾İ
+        DWORD res = WriteProcessMemory(INVALID_HANDLE_VALUE, (LPVOID)addr, &hp, 4, 0);  //å†™å…¥è‡ªèº«ä¿®æ”¹æ¸¸æˆæ•°æ®
         DWORD res2 = WriteProcessMemory(INVALID_HANDLE_VALUE, (LPVOID)addr2, &heart, 4, 0);
         DWORD res3 = WriteProcessMemory(INVALID_HANDLE_VALUE, (LPVOID)addr3, &life, 4, 0);
 
@@ -37,7 +37,7 @@ DWORD WINAPI inject(LPVOID) {
 BOOL APIENTRY DllMain(HMODULE hModule,  DWORD  ul_reason_for_call, LPVOID lpReserved) {
     switch(ul_reason_for_call) {
         case DLL_PROCESS_ATTACH: {
-            ::DisableThreadLibraryCalls(hModule);   //´´½¨Ïß³Ì°üº¬ËÀÑ­»·£¬Îª·À¿¨ËÀ±ØĞëÉèÖÃ
+            ::DisableThreadLibraryCalls(hModule);   //åˆ›å»ºçº¿ç¨‹åŒ…å«æ­»å¾ªç¯ï¼Œä¸ºé˜²å¡æ­»å¿…é¡»è®¾ç½®
             CreateThread(NULL, 0, inject, NULL, 0, NULL);
         }
         break;
